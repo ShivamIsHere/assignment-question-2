b47968e11dbb9e2d23bc8fb059de1b8fd768093f
Shivam <singhkumarshivam15@gmail.com>
2023-08-06 01:24:23 +0530

    first commit

--- src/component/highlightHTMLContent.js
+++ src/component/highlightHTMLContent.js
@@ -1,21 +1,17 @@
-function highlightHTMLContent(htmlContent, plainText, st,end) {
-
-  let array = [];
+function highlightHTMLContent(array,htmlContent, plainText, start,end) {
 
   let count = 0;
   let i = 0;
   let j = 0;
   
   while (i < htmlContent.length && j < plainText.length) {
-  
-   
-  
+
       if ((plainText[j] === " " )||(htmlContent[i] === plainText[j])) {
-          
-         
-      if(count===st){
+       
+      if(count===start){
           array.push("<mark>");
-      }else if(count===end){
+      }
+      if(count===end){
           array.push("</mark>");
       }
       count++;
@@ -33,12 +29,14 @@ function highlightHTMLContent(htmlContent, plainText, st,end) {
   array.push(htmlContent[i]);
   i++;
   }
+
   return array.join('');
 }
+
+
 let htmlContent = `<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>`;
 const plainText = "Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37 , and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here . -------------------------------------";
 const plainTextPositions = [
-  
   {
       start: 241,
       end: 247,
@@ -47,19 +45,12 @@ const plainTextPositions = [
         start: 518,
         end: 525,
     },
-    
 ];
 
 
+plainTextPositions.map((element)=>{
+  if(element.start<=element.end){
+htmlContent= (highlightHTMLContent([],htmlContent, plainText,element.start,element.end));
+}});
 
-plainTextPositions.map((ele)=>{
- 
- 
-  let st=ele.start;
-  let end=ele.end;
-let k= (highlightHTMLContent(htmlContent, plainText,st,end));
-htmlContent=k;
-
-
-})
-console.log(htmlContent)
+console.log(htmlContent);
